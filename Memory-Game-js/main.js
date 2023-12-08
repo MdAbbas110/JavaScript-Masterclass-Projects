5cfedb2ee5211502e95db390b2a47a409f97d8a3
Abbas <meerabbas110@gmail.com>
2023-12-08 21:20:59 +0530

    match the card logic is working

--- Memory-Game-js/main.js
+++ Memory-Game-js/main.js
@@ -22,6 +22,48 @@ function buildTile(color) {
   const element = document.createElement('div');
   element.classList.add('tile');
   element.setAttribute('data-color', color);
+  element.setAttribute('data-revealed', false);
+
+  element.addEventListener('click', () => {
+    // check that not to reveal tile on wrong select
+    if (awaitingEndOfMove) {
+      return;
+    }
+
+    element.style.backgroundColor = color;
+
+    if (!activeTile) {
+      activeTile = element;
+      return;
+    }
+
+    //* Logic to match the same colors
+
+    const colorToMatch = activeTile.getAttribute('data-color');
+
+    if (colorToMatch === color) {
+      awaitingEndOfMove = false;
+      activeTile = null;
+      revealedCount += 2;
+
+      if (revealedCount === tileCount) {
+        alert('You Win! Refresh to play again');
+      }
+      return;
+    }
+
+    // Down here
+    awaitingEndOfMove = true;
+
+    setTimeout(() => {
+      element.style.backgroundColor = null;
+      activeTile.style.backgroundColor = null;
+
+      awaitingEndOfMove = false;
+      activeTile = null;
+    }, 1000);
+  });
+
   return element;
 }
 
