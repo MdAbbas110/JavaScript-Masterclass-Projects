4ef9da82d0ba7b23ab4018c94500ca2d5982bf8e
Abbas <meerabbas110@gmail.com>
2023-12-26 17:25:46 +0530

    completed the format of guess the color game

--- guess-the-color/script.js
+++ guess-the-color/script.js
@@ -1,6 +1,8 @@
 const colorCodeContainer = document.getElementById('color-code');
 const optionsContainer = document.getElementById('options-container');
+const scoreContainer = document.getElementById('score');
 let randomColor = null;
+let score = 0;
 
 function generateRandomNumber(min, max) {
   return min + Math.floor(Math.random() * (max - min) + 1);
@@ -13,18 +15,33 @@ function generateRandomColorRGB() {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
+function incrementScore() {
+  score += 1;
+  scoreContainer.innerText = score;
+}
+
 function validateResult(el) {
   const selected = el.target.style.backgroundColor;
-  console.log(randomColor === selected);
+  if (randomColor === selected) {
+    incrementScore();
+  } else {
+    score = 0;
+  }
+
+  window.localStorage.setItem('score', score);
+  startGame();
 }
 
 function startGame() {
+  score = Number(window.localStorage.getItem('score') ?? 0);
+  scoreContainer.innerText = score;
+  optionsContainer.innerHTML = null;
   randomColor = generateRandomColorRGB();
   colorCodeContainer.innerText = randomColor;
 
   const ansIndex = generateRandomNumber(0, 5);
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     const div = document.createElement('div');
     div.addEventListener('click', validateResult);
 
